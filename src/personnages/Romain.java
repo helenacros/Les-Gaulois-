562fee3abd8f090aa85e580c36e59b0c7134d93b
helenacros <lna.cros@gmail.com>
2024-09-24 13:22:52 +0200

    les enumeres

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,18 +1,20 @@
 package personnages;
+
 public class Romain {
 	private String nom;
 	private int force;
-
+	private Equipement[] equipements;
+	private int nbEquipement=0;
 
 	public Romain(String nom, int force) { 
 		assert (force>=0);
 		this.nom = nom; 
 		this.force = force; 
+		equipements=new Equipement[2];
 	}
 
 	public void parler(String texte) {
 		System.out.println(prendreParole()+"�" + texte + "�");
- 
 	}
 	
 	private String prendreParole() {
@@ -28,7 +30,6 @@ public class Romain {
 		} else { 
 			parler("J'abandonne..."); }
 		assert (forceDebut>force);
-
 	}
 	
 	public String getNom() {
@@ -38,9 +39,36 @@ public class Romain {
 	@Override
 	public String toString() {
 		return "Romain [nom=" + nom + ", force=" + force + "]";
-	
 	}
 	
+	private void ajouterEquipement(Equipement equipement) {
+		equipements[nbEquipement]=equipement;
+		nbEquipement++;
+		System.out.println("Le soldat "+nom+"avec un "+equipement);
+	}
+	
+	
+	public void sEquiper(Equipement equipement) {	
+		switch(nbEquipement) {
+			
+			case 0 :
+				ajouterEquipement(equipement);
+				break;
+			
+			case 1 :
+				if (equipements[0]==equipement) {
+					System.out.println("Le soldat "+nom+" possède deja un "+equipement);
+				}
+				else {
+					ajouterEquipement(equipement);
+				}
+				break;
+				
+			case 2 :
+				System.out.println("Le soldat "+nom+" est deja bien protégé !");
+				break;	
+		}
+	}
 
 	public static void main(String[] args) {
 		Romain minus=new Romain("Minus",6);
@@ -48,10 +76,16 @@ public class Romain {
 		minus.parler("foutu gaulois");
 		minus.recevoirCoup(4);
 		System.out.println(minus);
+		
+		for (Equipement equipement : Equipement.values()){
+			System.out.println("-"+ equipement);
+		}
+				
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		
 	}
-	
-	
-	
-	
-	
+
 }
